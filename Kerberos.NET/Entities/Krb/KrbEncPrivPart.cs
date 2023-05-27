f58e874219f08c4d543b2cace1ae78462ad11f40
Leonid Korokh <lkorokh@gmail.com>
2023-05-27 16:11:02 +0300

    Add KRB-PRIV private part object

---
+++ Kerberos.NET/Entities/Krb/KrbEncPrivPart.cs
@@ -0,0 +1,18 @@
+﻿using Kerberos.NET.Asn1;
+
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.Asn1;
+using System.Text;
+
+namespace Kerberos.NET.Entities
+{
+    public partial class KrbEncPrivPart : IAsn1ApplicationEncoder<KrbEncPrivPart>
+    {
+        public KrbEncPrivPart DecodeAsApplication(ReadOnlyMemory<byte> data)
+        {
+            return DecodeApplication (data);
+        }
+
+    }
+}
---
+++ Kerberos.NET/Entities/Krb/KrbEncPrivPart.generated.cs
@@ -0,0 +1,263 @@
+﻿// -----------------------------------------------------------------------
+// Licensed to The .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// -----------------------------------------------------------------------
+
+// This is a generated file.
+// The generation template has been modified from .NET Runtime implementation
+
+using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Asn1;
+using Kerberos.NET.Crypto;
+using Kerberos.NET.Asn1;
+
+namespace Kerberos.NET.Entities
+{
+    public partial class KrbEncPrivPart
+    {
+        /*
+        EncKrbPrivPart ::=   [APPLICATION 28] SEQUENCE {
+                user-data[0]              OCTET STRING,
+                timestamp[1]              KerberosTime OPTIONAL,
+                usec[2]                   INTEGER OPTIONAL,
+                seq-number[3]             INTEGER OPTIONAL,
+                s-address[4]              HostAddress,
+                r-address[5]              HostAddress OPTIONAL                                                      
+   }
+         */
+    
+        public ReadOnlyMemory<byte> UserData { get; set; }
+  
+        public DateTimeOffset? Timestamp { get; set; }
+  
+        public int? USec { get; set; }
+  
+        public int? SequenceNumber { get; set; }
+  
+        public KrbHostAddress SAddr { get; set; }
+  
+        public KrbHostAddress RAddr { get; set; }
+  
+        // Encoding methods
+        internal void Encode(AsnWriter writer)
+        {
+            EncodeApplication(writer, ApplicationTag);
+        }
+        
+        internal void Encode(AsnWriter writer, Asn1Tag tag)
+        {
+            writer.PushSequence(tag);
+            
+            writer.PushSequence(new Asn1Tag(TagClass.ContextSpecific, 0));
+            writer.WriteOctetString(UserData.Span);
+            writer.PopSequence(new Asn1Tag(TagClass.ContextSpecific, 0));
+
+            if (Asn1Extension.HasValue(Timestamp))
+            {
+                writer.PushSequence(new Asn1Tag(TagClass.ContextSpecific, 1));
+                writer.WriteGeneralizedTime(Timestamp.Value);
+                writer.PopSequence(new Asn1Tag(TagClass.ContextSpecific, 1));
+            }
+
+            if (Asn1Extension.HasValue(USec))
+            {
+                writer.PushSequence(new Asn1Tag(TagClass.ContextSpecific, 2));
+                writer.WriteInteger(USec.Value);
+                writer.PopSequence(new Asn1Tag(TagClass.ContextSpecific, 2));
+            }
+
+            if (Asn1Extension.HasValue(SequenceNumber))
+            {
+                writer.PushSequence(new Asn1Tag(TagClass.ContextSpecific, 3));
+                writer.WriteInteger(SequenceNumber.Value);
+                writer.PopSequence(new Asn1Tag(TagClass.ContextSpecific, 3));
+            }
+
+            if (Asn1Extension.HasValue(SAddr))
+            {
+                writer.PushSequence(new Asn1Tag(TagClass.ContextSpecific, 4));
+                SAddr?.Encode(writer);
+                writer.PopSequence(new Asn1Tag(TagClass.ContextSpecific, 4));
+            }
+
+            if (Asn1Extension.HasValue(RAddr))
+            {
+                writer.PushSequence(new Asn1Tag(TagClass.ContextSpecific, 5));
+                RAddr?.Encode(writer);
+                writer.PopSequence(new Asn1Tag(TagClass.ContextSpecific, 5));
+            }
+            writer.PopSequence(tag);
+        }
+        
+        internal void EncodeApplication(AsnWriter writer, Asn1Tag tag)
+        {
+            writer.PushSequence(tag);
+            
+            this.Encode(writer, Asn1Tag.Sequence);
+            
+            writer.PopSequence(tag);
+        }       
+        
+        private static readonly Asn1Tag ApplicationTag = new Asn1Tag(TagClass.Application, 28);
+        
+        public virtual ReadOnlyMemory<byte> EncodeApplication() 
+        {
+          return EncodeApplication(ApplicationTag);
+        }
+        
+        public static KrbEncPrivPart DecodeApplication(ReadOnlyMemory<byte> encoded)
+        {
+            AsnReader reader = new AsnReader(encoded, AsnEncodingRules.DER);
+
+            var sequence = reader.ReadSequence(ApplicationTag);
+          
+            KrbEncPrivPart decoded;
+            Decode(sequence, Asn1Tag.Sequence, out decoded);
+            sequence.ThrowIfNotEmpty();
+
+            reader.ThrowIfNotEmpty();
+
+            return decoded;
+        }
+        
+        internal static KrbEncPrivPart DecodeApplication<T>(AsnReader reader, out T decoded)
+          where T: KrbEncPrivPart, new()
+        {
+            var sequence = reader.ReadSequence(ApplicationTag);
+          
+            Decode(sequence, Asn1Tag.Sequence, out decoded);
+            sequence.ThrowIfNotEmpty();
+
+            reader.ThrowIfNotEmpty();
+
+            return decoded;
+        }
+         
+        internal ReadOnlyMemory<byte> EncodeApplication(Asn1Tag tag)
+        {
+            using (var writer = new AsnWriter(AsnEncodingRules.DER))
+            {
+                EncodeApplication(writer, tag);
+
+                return writer.EncodeAsMemory();
+            }
+        }
+        
+        internal static KrbEncPrivPart Decode(Asn1Tag expectedTag, ReadOnlyMemory<byte> encoded)
+        {
+            AsnReader reader = new AsnReader(encoded, AsnEncodingRules.DER);
+            
+            Decode(reader, expectedTag, out KrbEncPrivPart decoded);
+            reader.ThrowIfNotEmpty();
+            return decoded;
+        }
+
+        internal static KrbEncPrivPart Decode(Asn1Tag expectedTag, ReadOnlyMemory<byte> encoded, AsnEncodingRules ruleSet)
+        {
+            AsnReader reader = new AsnReader(encoded, ruleSet);
+            
+            Decode(reader, expectedTag, out KrbEncPrivPart decoded);
+            reader.ThrowIfNotEmpty();
+            return decoded;
+        }
+
+        internal static void Decode<T>(AsnReader reader, out T decoded)
+          where T: KrbEncPrivPart, new()
+        {
+            if (reader == null)
+            {
+                throw new ArgumentNullException(nameof(reader));
+            }
+            
+            DecodeApplication(reader, out decoded);
+        }
+
+        internal static void Decode<T>(AsnReader reader, Asn1Tag expectedTag, out T decoded)
+          where T: KrbEncPrivPart, new()
+        {
+            if (reader == null)
+            {
+                throw new ArgumentNullException(nameof(reader));
+            }
+
+            decoded = new T();
+            
+            AsnReader sequenceReader = reader.ReadSequence(expectedTag);
+            AsnReader explicitReader;
+            
+            explicitReader = sequenceReader.ReadSequence(new Asn1Tag(TagClass.ContextSpecific, 0));
+
+            if (explicitReader.TryReadPrimitiveOctetStringBytes(out ReadOnlyMemory<byte> tmpUserData))
+            {
+                decoded.UserData = tmpUserData;
+            }
+            else
+            {
+                decoded.UserData = explicitReader.ReadOctetString();
+            }
+
+            explicitReader.ThrowIfNotEmpty();
+
+            if (sequenceReader.HasData && sequenceReader.PeekTag().HasSameClassAndValue(new Asn1Tag(TagClass.ContextSpecific, 1)))
+            {
+                explicitReader = sequenceReader.ReadSequence(new Asn1Tag(TagClass.ContextSpecific, 1));                
+            
+                decoded.Timestamp = explicitReader.ReadGeneralizedTime();
+                explicitReader.ThrowIfNotEmpty();
+            }
+
+            if (sequenceReader.HasData && sequenceReader.PeekTag().HasSameClassAndValue(new Asn1Tag(TagClass.ContextSpecific, 2)))
+            {
+                explicitReader = sequenceReader.ReadSequence(new Asn1Tag(TagClass.ContextSpecific, 2));                
+            
+                if (explicitReader.TryReadInt32(out int tmpUSec))
+                {
+                    decoded.USec = tmpUSec;
+                }
+                else
+                {
+                    explicitReader.ThrowIfNotEmpty();
+                }
+
+                explicitReader.ThrowIfNotEmpty();
+            }
+
+            if (sequenceReader.HasData && sequenceReader.PeekTag().HasSameClassAndValue(new Asn1Tag(TagClass.ContextSpecific, 3)))
+            {
+                explicitReader = sequenceReader.ReadSequence(new Asn1Tag(TagClass.ContextSpecific, 3));                
+            
+                if (explicitReader.TryReadInt32(out int tmpSequenceNumber))
+                {
+                    decoded.SequenceNumber = tmpSequenceNumber;
+                }
+                else
+                {
+                    explicitReader.ThrowIfNotEmpty();
+                }
+
+                explicitReader.ThrowIfNotEmpty();
+            }
+
+            if (sequenceReader.HasData && sequenceReader.PeekTag().HasSameClassAndValue(new Asn1Tag(TagClass.ContextSpecific, 4)))
+            {
+                explicitReader = sequenceReader.ReadSequence(new Asn1Tag(TagClass.ContextSpecific, 4));                
+            
+                KrbHostAddress.Decode<KrbHostAddress>(explicitReader, out KrbHostAddress tmpSAddr);
+                decoded.SAddr = tmpSAddr;
+                explicitReader.ThrowIfNotEmpty();
+            }
+
+            if (sequenceReader.HasData && sequenceReader.PeekTag().HasSameClassAndValue(new Asn1Tag(TagClass.ContextSpecific, 5)))
+            {
+                explicitReader = sequenceReader.ReadSequence(new Asn1Tag(TagClass.ContextSpecific, 5));                
+            
+                KrbHostAddress.Decode<KrbHostAddress>(explicitReader, out KrbHostAddress tmpRAddr);
+                decoded.RAddr = tmpRAddr;
+                explicitReader.ThrowIfNotEmpty();
+            }
+
+            sequenceReader.ThrowIfNotEmpty();
+        }
+    }
+}
