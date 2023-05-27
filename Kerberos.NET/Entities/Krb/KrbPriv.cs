d2a4daabd60cf0c816148a752dad0cafe2557ab4
Leonid Korokh <lkorokh@gmail.com>
2023-05-27 16:11:43 +0300

    Add KRB-PRIV message object

---
+++ Kerberos.NET/Entities/Krb/KrbPriv.cs
@@ -0,0 +1,20 @@
+﻿using Kerberos.NET.Asn1;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kerberos.NET.Entities
+{
+    public partial class KrbPriv : IAsn1ApplicationEncoder<KrbPriv>
+    {
+        public KrbPriv() {
+            ProtocolVersionNumber = 5;
+            MessageType = MessageType.KRB_PRIV;
+        }
+        public KrbPriv DecodeAsApplication(ReadOnlyMemory<byte> data)
+        {
+            return DecodeApplication(data);
+        }
+    }
+}
